cf9604fd49a8e1543652d1ef76aab337e9b72e0c
Ernest Kamau <1.kamauernest@gmail.com>
2025-10-24 16:01:51 +0300

    Adding payments table

--- database/migrations/2025_10_24_121927_create_payments_table.php
+++ database/migrations/2025_10_24_121927_create_payments_table.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('sale_id')->constrained('sales')->cascadeOnDelete();
+            
+            $table->enum('method', ['cash', 'mpesa', 'bank_transfer', 'card']);
+            $table->decimal('amount', 10, 2)->default(0.00);
+            
+            // M-Pesa transaction code, bank reference, etc.
+            $table->string('reference', 100)->nullable();
+            $table->text('notes')->nullable();
+
+            $table->timestamp('paid_at')->useCurrent(); // Auto-set to now
             $table->timestamps();
+            
+
+            $table->index('sale_id');
+            $table->index('method');
+            $table->index('paid_at'); // Date range reports
+
         });
     }
 
